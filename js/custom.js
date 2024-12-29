193e2c7e0e730cfdc60f32fa4a456241db49fe50
vlknhlc <volkanhalici@gmail.com>
2024-12-29 17:46:57 +0300

    save6

--- js/custom.js
+++ js/custom.js
@@ -5,16 +5,4 @@
 })();
 
 // google siteyi anlama kodu
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "EMV YAZILIM",
-  "url": "https://www.emvyazilim.com.tr",
-  "logo": "https://emvyazilim.com.tr/logo.png",
-  "sameAs": [
-    "https://www.facebook.com/siteniz",
-    "https://www.instagram.com/siteniz"
-  ]
-}
-</script>
+

022be500075283f96c6b69c56b4825c3df81e3e5
vlknhlc <volkanhalici@gmail.com>
2024-12-29 17:38:05 +0300

    save4

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,18 @@
     var year = new Date().getFullYear();
     document.querySelector("#currentYear").innerHTML = year;
 })();
+
+// google siteyi anlama kodu
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "EMV YAZILIM",
+  "url": "https://www.emvyazilim.com.tr",
+  "logo": "https://emvyazilim.com.tr/logo.png",
+  "sameAs": [
+    "https://www.facebook.com/siteniz",
+    "https://www.instagram.com/siteniz"
+  ]
+}
+</script>
